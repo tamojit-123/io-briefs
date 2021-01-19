43fe65b0d8186be502689d29c428e822ba1ebd35
Tamojit <tamo.das.97@gmail.com>
2021-01-19 23:23:48 +0530

    Updated Blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,80 +1,99 @@
 require(`dotenv`).config({
-  path: `.env`,
+    path: `.env`,
 })
 
 const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
-  siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-  },
-  plugins: [
-    {
-      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
-      // See the theme's README for all available options
-      options: {
-        navigation: [
-          {
-            title: `Blog`,
-            slug: `/blog`,
-          },
-          {
-            title: `About`,
-            slug: `/about`,
-          },
-        ],
-        externalLinks: [
-          {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
-          },
-          {
-            name: `Instagram`,
-            url: `https://www.instagram.com/lekoarts.de/`,
-          },
-        ],
-      },
+    siteMetadata: {
+        siteImage: './static/favicon-32x32.png',
+        siteTitle: `⚙ I / O Briefs`,
+        siteTitleAlt: `I / O Briefs`,
+        siteHeadline: `⚙ I / O Briefs - Delivers guides for curious minds.`,
+        // Will be used to generate absolute URLs for og:image etc.
+        siteUrl: `https://minimal-blog.lekoarts.de`,
+        // Used for SEO
+        siteDescription: `Supporting developers at every stage. Deliver guides for curious minds.`,
+        // Will be set on the <html /> tag
+        siteLanguage: `en`,
+        // Twitter Handle
+        author: `@tamojitdas97`,
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
-    },
-    `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#6B46C1`,
-        display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`,
-    shouldAnalyseBundle && {
-      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
-      options: {
-        analyzerMode: `static`,
-        reportFilename: `_bundle.html`,
-        openAnalyzer: false,
-      },
-    },
-  ].filter(Boolean),
+    plugins: [
+        {
+            resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+            // See the theme's README for all available options
+            options: {
+                navigation: [
+                    {
+                        title: `Blog`,
+                        slug: `/blog`,
+                    },
+                    {
+                        title: `About`,
+                        slug: `/about`,
+                    },
+                    {
+                        title: `About Me`,
+                        slug: `/aboutme`,
+                    },
+                ],
+                externalLinks: [
+                    {
+                        name: `Twitter`,
+                        url: `https://twitter.com/tamojitdas97`,
+                    },
+                    {
+                        name: `LinkedIn`,
+                        url: `https://www.linkedin.com/in/tamojit-das-365b19118/`,
+                    },
+                    {
+                        name: `GitHub`,
+                        url: `https://github.com/tamojit-123`,
+                    },
+                ],
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: process.env.GOOGLE_ANALYTICS_ID,
+            },
+        },
+        `gatsby-plugin-sitemap`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
+                short_name: `minimal-blog`,
+                description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+                start_url: `/`,
+                background_color: `#fff`,
+                theme_color: `#6B46C1`,
+                display: `standalone`,
+                icons: [
+                    {
+                        src: `/android-chrome-192x192.png`,
+                        sizes: `192x192`,
+                        type: `image/png`,
+                    },
+                    {
+                        src: `/android-chrome-512x512.png`,
+                        sizes: `512x512`,
+                        type: `image/png`,
+                    },
+                ],
+            },
+        },
+        `gatsby-plugin-offline`,
+        `gatsby-plugin-netlify`,
+        shouldAnalyseBundle && {
+            resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+            options: {
+                analyzerMode: `static`,
+                reportFilename: `_bundle.html`,
+                openAnalyzer: false,
+            },
+        },
+    ].filter(Boolean),
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,44 @@
+/** @jsx jsx */
+import { jsx, Link } from "theme-ui"
+import useSiteMetadata from "../hooks/use-site-metadata"
+
+const Footer = () => {
+  const { siteTitle } = useSiteMetadata()
+
+  return (
+    <footer
+      sx={{
+        boxSizing: `border-box`,
+        display: `flex`,
+        justifyContent: `space-between`,
+        mt: [6],
+        color: `secondary`,
+        a: {
+          variant: `links.secondary`,
+        },
+        flexDirection: [`column`, `column`, `row`],
+        variant: `dividers.top`,
+      }}
+    >
+      <div>
+        &copy; {new Date().getFullYear()} by {siteTitle}. All rights reserved.
+      </div>
+      <div>
+        <Link
+          aria-label="Link to GitHub"
+          href="https://github.com/tamojit-123"
+        >
+          Develop
+        </Link>
+        {` `}
+        by
+        {` `}
+        <Link aria-label="Link to the theme author's website" href="https://tamojitdas.netlify.app/">
+          Tamojit Das
+        </Link>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
